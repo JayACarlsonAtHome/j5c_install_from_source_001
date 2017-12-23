2dd3f6769450826d0960c645bed267a79ff170b1
Jay A Carlson <jay.a.carlson@gmail.com>
2017-12-23 00:54:39 -0800

    Install Perl Added

---
+++ cmake-build-debug/CMakeFiles/Install.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Install.dir/main.cpp.o"
+  "Install.pdb"
+  "Install"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Install.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
