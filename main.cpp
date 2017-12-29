bcdd50c2f3672d8990198891c3234e440e06a11d
Jay A Carlson <jay.a.carlson@gmail.com>
2017-12-29 03:43:29 -0800

    Improved (fixed) tcl/tk load

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include <vector>
 #include <stdio.h>
 
-enum class Os_type{Linux_Mint = 0, CentOS = 1, RedHat = 2, MaxOSX = 3};
+enum class OS_type { Selection_Not_Available = -1, No_Selection_Made = 0, Linux_Mint = 1, CentOS = 2, RedHat = 3, MaxOSX = 4};
+enum class Mac_PM  { Selection_Not_Available = -1, No_Selection_Made = 0, Home_Brew = 0, MacPorts = 1 };
 
 using sstr = std::string;
 
@@ -14,11 +15,15 @@ int do_command(std::vector<sstr>& vec)
     int result = 0;
     for (auto it = vec.cbegin(); it != vec.cend(); ++it )
     {
+        result = 0;
         command =  *it;
         std::cout << "Command: " << command << std::endl;
-        result = system(command.c_str());
-        //result = 0;
-        if ((result != 0) && (!(command.substr(0,3) == "yum")))
+        if (command.substr(0,1) != "#")
+        {
+            result = system(command.c_str());
+        }
+
+        if ((result != 0) && (!((command.substr(0,3) == "apt") || (command.substr(0,3) == "yum"))) )
         {
             std::cout << "!!!Error -- Process terminated for safety...";
             break;
@@ -27,15 +32,32 @@ int do_command(std::vector<sstr>& vec)
     return result;
 }
 
-int install_required_dependencies()
+int install_apt_required_dependencies()
 {
-    sstr federa_release = "7";
+    std::cout << "Not Implemented Yet" << std::endl;
+    return 1;
+}
+
+int install_mac_required_dependencies(Mac_PM mpm)
+{
+    if (mpm == Mac_PM ::Home_Brew) {
+        //install_home_brew_required_dependencies();
+    }
+    if (mpm == Mac_PM ::MacPorts) {
+        //install_macPort_required_dependencies();
+    }
+    return 1;
+}
+
+int install_yum_required_dependencies()
+{
+    sstr fedora_release = "7";
     sstr command = "";
     std::vector<sstr> vec;
     vec.push_back("yum -y install wget");
-    vec.push_back("wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-" + federa_release + ".noarch.rpm");
-    vec.push_back("yum -y install epel-release-latest-" + federa_release +".noarch.rpm");
-    vec.push_back("rm -f epel-release-latest-7.noarch.rpm");
+    vec.push_back("wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-" + fedora_release + ".noarch.rpm");
+    vec.push_back("yum -y install epel-release-latest-" + fedora_release +".noarch.rpm");
+    vec.push_back("rm -f epel-release-latest-" + fedora_release +".noarch.rpm");
     vec.push_back("yum -y install autoconf");
     vec.push_back("yum -y install bison");
     vec.push_back("yum -y install boost-devel");
@@ -82,6 +104,8 @@ int install_required_dependencies()
     return result;
 }
 
+
+
 int install_perl(sstr& path, sstr& usrPath)
 {
     sstr command = "";
@@ -96,63 +120,62 @@ int install_perl(sstr& path, sstr& usrPath)
     vec.push_back("eval \"cd " + path + "/perl-5.26.1; make \"");
     vec.push_back("eval \"cd " + path + "/perl-5.26.1; make test \"");
     vec.push_back("eval \"cd " + path + "/perl-5.26.1; make install \"");
+    vec.push_back("eval \"cd /j5c\"");
     int result = do_command(vec);
     return result;
 }
 
-int install_tcl(sstr& path, sstr& usrPath, Os_type thisOS)
+int install_tcl(sstr& path, sstr& usrPath, OS_type thisOS, sstr& version)
 {
     sstr command = "";
+    sstr installOS = "";
     std::vector<sstr> vec;
-    vec.push_back("eval \"mkdir -p " + path + "\"");
-    vec.push_back("eval \"wget https://prdownloads.sourceforge.net/tcl/tcl8.6.7-src.tar.gz\"");
-    vec.push_back("eval \"cp ./tcl8.6.7-src.tar.gz " + path + "\"");
-    vec.push_back("eval \"rm -f ./tcl8.6.7-src.tar.gz \"");
-    vec.push_back("eval \"cd " + path + "; tar xvf tcl8.6.7-src.tar.gz\"");
-    vec.push_back("eval \"mkdir -p " + usrPath + "\"");
-    if ((thisOS == Os_type::Linux_Mint)
-        || (thisOS == Os_type::CentOS)
-        || (thisOS == Os_type::RedHat))
-    {
-        vec.push_back("eval \"cd " + path + "/tcl8.6.7/unix; ./configure --prefix="  + usrPath + " --enable-threads --enable-shared --enable-symbols;  \"");
-    }
 
-    if (thisOS == Os_type::MaxOSX)
-    {
-        vec.push_back("eval \"cd " + path + "/tcl8.6.7/macosx; ./configure --prefix=" + usrPath + " --enable-threads --enable-shared --enable-symbols;  \"");
-    }
-    vec.push_back("eval \"cd " + path + "/tcl8.6.7; make \"");
-    vec.push_back("eval \"cd " + path + "/tcl8.6.7; make test \"");
-    vec.push_back("eval \"cd " + path + "/tcl8.6.7; make install \"");
+    if (thisOS == OS_type::RedHat)      installOS = "unix";
+    if (thisOS == OS_type::Linux_Mint)  installOS = "unix";
+    if (thisOS == OS_type::MaxOSX)      installOS = "macosx";
+
+    vec.push_back("eval \"mkdir -p " + path + "\"");
+    vec.push_back("eval \"wget https://prdownloads.sourceforge.net/tcl/tcl" + version + "-src.tar.gz\"");
+    vec.push_back("eval \"cp ./tcl" + version + "-src.tar.gz " + path + "\"");
+    vec.push_back("eval \"rm -f ./tcl" + version + "-src.tar.gz \"");
+    vec.push_back("eval \"cd " + path + "; tar xvf tcl" + version + "-src.tar.gz\"");
+    vec.push_back("eval \"mkdir -p " + usrPath + "\"");
+    vec.push_back("eval \"cd " + path + "/tcl" +version + "/" + installOS
+                  + "; ./configure --prefix="  + usrPath + " --enable-threads --enable-shared --enable-symbols;  \"");
+    vec.push_back("eval \"cd " + path + "/tcl" +version + "/" + installOS + "; make \"");
+    vec.push_back("eval \"cd " + path + "/tcl" +version + "/" + installOS + "; make test \"");
+    vec.push_back("eval \"cd " + path + "/tcl" +version + "/" + installOS + "; make install \"");
+    vec.push_back("eval \"cd /j5c\"");
     int result = do_command(vec);
     return result;
 }
 
 
-int install_tk(sstr& path, sstr& usrPath, Os_type thisOS, sstr& oldPath)
+int install_tk(sstr& path, sstr& usrPath, OS_type thisOS, sstr& oldPath, sstr& version)
 {
     sstr command = "";
+    sstr installOS = "";
     std::vector<sstr> vec;
-    vec.push_back("eval \"mkdir -p " + path + "\"");
-    vec.push_back("eval \"wget https://prdownloads.sourceforge.net/tcl/tk8.6.7-src.tar.gz\"");
-    vec.push_back("eval \"cp ./tk8.6.7-src.tar.gz " + path + "\"");
-    vec.push_back("eval \"rm -f ./tk8.6.7-src.tar.gz \"");
-    vec.push_back("eval \"cd " + path + "; tar xvf tk8.6.7-src.tar.gz\"");
-    vec.push_back("eval \"mkdir -p " + usrPath + "\"");
-    if ((thisOS == Os_type::Linux_Mint)
-        || (thisOS == Os_type::CentOS)
-        || (thisOS == Os_type::RedHat))
-    {
-        vec.push_back("eval \"cd " + path + "/tk8.6.7/unix; ./configure --prefix="  + usrPath + " --with-tcl=" + oldPath + "/tcl8.6.7/unix --enable-threads --enable-shared --enable-symbols;  \"");
-    }
 
-    if (thisOS == Os_type::MaxOSX)
-    {
-        vec.push_back("eval \"cd " + path + "/tk8.6.7/macosx; ./configure --prefix=" + usrPath + " --with-tcl=" + oldPath + "/tcl8.6.7/macosx --enable-threads --enable-shared --enable-symbols;  \"");
-    }
-    vec.push_back("eval \"cd " + path + "/tk8.6.7/unix; make \"");
-    vec.push_back("eval \"cd " + path + "/tk8.6.7/unix; make test \"");
-    vec.push_back("eval \"cd " + path + "/tk8.6.7/unix; make install \"");
+    if (thisOS == OS_type::RedHat)      installOS = "unix";
+    if (thisOS == OS_type::Linux_Mint)  installOS = "unix";
+    if (thisOS == OS_type::MaxOSX)      installOS = "macosx";
+
+    vec.push_back("eval \"mkdir -p " + path + "\"");
+    vec.push_back("eval \"wget https://prdownloads.sourceforge.net/tcl/tk" + version +"-src.tar.gz\"");
+    vec.push_back("eval \"cp ./tk" + version + "-src.tar.gz " + path + "\"");
+    vec.push_back("eval \"rm -f ./tk"+ version +"-src.tar.gz \"");
+    vec.push_back("eval \"cd " + path + "; tar xvf tk" + version + "-src.tar.gz\"");
+    vec.push_back("eval \"mkdir -p " + usrPath + "\"");
+    vec.push_back("eval \"cd " + path + "/tk" + version + "/" + installOS
+                  + "; ./configure --prefix="  + usrPath + " --with-tcl=" + oldPath + "/tcl" + version +"/" + installOS
+                  + " --enable-threads --enable-shared --enable-symbols;  \"");
+    vec.push_back("eval \"cd " + path + "/tk" + version + "/" + installOS + "; make \"");
+    vec.push_back("eval \"cd " + path + "/tk" + version + "/" + installOS + "; make test \"");
+    vec.push_back("eval \"cd " + path + "/tk" + version + "/" + installOS + "; make install \"");
+
+    vec.push_back("eval \"cd /j5c\"");
     int result = do_command(vec);
     return result;
 }
@@ -222,19 +245,34 @@ bool File_Contents(std::ofstream& file, sstr& fileName, std::vector<sstr>& vec_l
 sstr setPath(sstr& company, sstr& prod_PathOffset, sstr& programName)
 {
     return company + prod_PathOffset + "/" + programName;
-
 };
 
 sstr setUsrPath(sstr& company, sstr& prod_Usr_Offset, sstr& programName)
 {
     return company + prod_Usr_Offset + "/" + programName;
 };
+
+
 int main()
 {
-    Os_type thisOS = Os_type ::RedHat;
+    OS_type thisOS = OS_type::RedHat;
+    Mac_PM  mpm    = Mac_PM ::Selection_Not_Available;
 
-    //ensure dependencies are met
-    install_required_dependencies();
+
+    if (thisOS == OS_type::RedHat)
+    {
+        install_yum_required_dependencies();
+    }
+
+    if (thisOS == OS_type::Linux_Mint)
+    {
+        install_apt_required_dependencies();
+    }
+
+    if (thisOS == OS_type::MaxOSX)
+    {
+        install_mac_required_dependencies(mpm);
+    }
 
     //basic setup
     sstr company = "/j5c";
@@ -252,23 +290,24 @@ int main()
     path = setPath(company, prod_PathOffset, programName);
     usrPath = setUsrPath(company, prod_Usr_Offset, programName);
     result = install_perl(path, usrPath);
-    std::cout << "Install Perl result = " << result << ".";
+    std::cout << "Install Perl result = " << result << "." << std::endl;
 
     //tcl setup
     programName = "tcl";
+    version     = "8.6.8";
     path = setPath(company, prod_PathOffset, programName);
     usrPath = setUsrPath(company, prod_Usr_Offset, programName);
-    result = install_tcl(path, usrPath, thisOS);
-    std::cout << "Install Tcl result = " << result << ".";
+    result = install_tcl(path, usrPath, thisOS, version);
+    std::cout << "Install Tcl result = " << result << "." << std::endl;
 
     //tk setup
     programName = "tk";
+    version     = "8.6.8";
     sstr oldPath = path;
     path = setPath(company, prod_PathOffset, programName);
     usrPath = setUsrPath(company, prod_Usr_Offset, programName);
-    result = install_tk(path, usrPath, thisOS, oldPath);
-    std::cout << "Install Tk result = " << result << ".";
-
+    result = install_tk(path, usrPath, thisOS, oldPath, version);
+    std::cout << "Install Tk result = " << result << "." << std::endl;
 
     return 0;
 }
